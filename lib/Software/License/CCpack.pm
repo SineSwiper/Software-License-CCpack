9470db64bfe41c85260c9a66c50907c84921665f
Brendan Byrd <Perl@ResonatorSoft.org>
2014-10-19 10:46:05 -0400

    Fix links in POD and add x_IRC resource

--- lib/Software/License/CCpack.pm
+++ lib/Software/License/CCpack.pm
@@ -25,11 +25,11 @@ __END__
 = DESCRIPTION
 
 This "license pack" contains all of the licenses from Creative Commons,
-except for CC0, which is already included in L<Software::License>.
+except for CC0, which is already included in [Software::License].
 
 Note that I don't recommend using these licenses for your own CPAN
 modules.  (Most of the licenses aren't even compatible with CPAN.)
-However, S:L modules are useful for more than mere L<CPAN::Meta::license>
+However, S:L modules are useful for more than mere [CPAN::Meta]->license
 declaration, so these modules exist for those other purposes.
 
 =end wikidoc
