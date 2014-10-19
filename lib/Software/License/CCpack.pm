ab52f8589636a7b315005f8d1a96b43854b1342b
Brendan Byrd <Perl@ResonatorSoft.org>
2014-10-19 00:59:55 -0400

    Basic cleanup of CCpack.pm

--- lib/Software/License/CCpack.pm
+++ lib/Software/License/CCpack.pm
@@ -1,21 +1,28 @@
 package Software::License::CCpack;
 
-use strict;
-use warnings;
-
-1;
-
-# PODNAME: Software::License::CCpack
+# AUTHORITY
+# VERSION
 # ABSTRACT: Software::License pack for Creative Commons' licenses
-=head1 SYNOPSIS
+
+42;
+
+__END__
+
+=encoding utf8
+
+=begin wikidoc
+
+= SYNOPSIS
+
+   use Software::License::CC_BY_4_0;
 
    my $license = Software::License::CC_BY_4_0->new({
       holder => 'Brendan Byrd',
    });
 
-   say $license->fulltext;
+   print $license->fulltext;
 
-=head1 DESCRIPTION
+= DESCRIPTION
 
 This "license pack" contains all of the licenses from Creative Commons,
 except for CC0, which is already included in L<Software::License>.
@@ -25,4 +32,4 @@ modules.  (Most of the licenses aren't even compatible with CPAN.)
 However, S:L modules are useful for more than mere L<CPAN::Meta::license>
 declaration, so these modules exist for those other purposes.
 
-=cut
+=end wikidoc
